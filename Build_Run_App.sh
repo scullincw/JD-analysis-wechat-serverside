a0175def4f8a19d2b43325b45ab521d3e9760889
高速啤机 <42400404+scullincw@users.noreply.github.com>
2020-03-11 20:24:22 +0800

    Update Build_Run_App.sh
    测试 Linux Shell

--- Build_Run_App.sh
+++ Build_Run_App.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo '======= Start building and runing SpringBoot Application ======='
 # 杀死已有进程
-kill -9 $(ps aux|grep wechat-serverside-0.0.1-SNAPSHOT.jar) &&
-sleep 5s
+kill -9 $(ps aux|grep wechat-serverside-0.0.1-SNAPSHOT.jar)
+sleep 2s
 # 切换目录
 cd /root/JD-analysis-wechat-serverside &&
 # 从GitHub仓库拉取最新分支
