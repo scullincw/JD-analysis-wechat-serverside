28ae518840f77591063773bf81ab35ee20ba0554
Scullincw <1148135455@qq.com>
2020-03-11 22:16:53 +0800

    update main.yml and setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-echo '======= Start building and runing SpringBoot Application =======\n'
+#echo '======= Start building and runing SpringBoot Application =======\n'
 # 杀死已有进程
 kill -9 $(ps aux|grep wechat-serverside-0.0.1-SNAPSHOT.jar) || echo 'Nothing to kill' >log/$(datatime).log 2>&1
 sleep 2s
 
 # 切换目录
-cd JD-analysis-wechat-serverside || echo 'cd fail' >log/$(datatime).log 2>&1
+#cd JD-analysis-wechat-serverside || echo 'cd fail' >log/$(datatime).log 2>&1
 
 # 从GitHub仓库拉取最新分支
-git fetch --all || echo 'git fetch fail' >log/$(datatime).log 2>&1
-git reset --hard origin/master || echo 'git reset fail' >log/$(datatime).log 2>&1
-git status >log/$(datatime).log 2>&1
+#git fetch --all || echo 'git fetch fail' >log/$(datatime).log 2>&1
+#git reset --hard origin/master || echo 'git reset fail' >log/$(datatime).log 2>&1
+#git status >log/$(datatime).log 2>&1
 
 # 清理项目
 mvn clean >log/$(datatime).log 2>&1
