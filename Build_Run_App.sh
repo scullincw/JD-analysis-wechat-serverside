d2b052ddd2f703d2e60cf18998c0b030ab41ae64
Scullincw <1148135455@qq.com>
2020-03-11 18:42:01 +0800

    编写了自动化部署项目的Linux Shell

--- Build_Run_App.sh
+++ Build_Run_App.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
-#curl -X POST localhost:8081/shutdown
-cd /root/JD-analysis-wechat-serverside && git status && git pull && mvn clean && mvn install -Dmaven.test.skip=true && cd target && nohup java -jar wechat-serverside-0.0.1-SNAPSHOT.jar &
+echo '======= Start building and runing SpringBoot Application ======='
+# 杀死已有进程
+kill -9 $(ps aux|grep wechat-serverside-0.0.1-SNAPSHOT.jar) &&
+sleep 5s
+# 切换目录
+cd /root/JD-analysis-wechat-serverside &&
+# 从GitHub仓库拉取最新分支
+git pull &&
+# 清理项目
+mvn clean &&
+# 安装项目
+mvn install -Dmaven.test.skip=true &&
+# 运行项目
+cd target &&
+nohup java -jar wechat-serverside-0.0.1-SNAPSHOT.jar &
